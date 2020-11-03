0b2fbc5efe6b6bfabbe21ca139a367acb34ddf69
Zethson <lukas.heumos@posteo.net>
2020-11-03 13:49:48 +0100

    fixed merge conflicts
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- mlf_core/create/templates/package/package_prediction/hooks/post_gen_project.py
+++ mlf_core/create/templates/package/package_prediction/hooks/post_gen_project.py
@@ -9,19 +9,27 @@ def remove(filepath):
         shutil.rmtree(filepath)
 
 
-xgboost_files = ['{{ cookiecutter.project_slug_no_hyphen }}/{{ cookiecutter.project_slug_no_hyphen }}/cli_xgboost.py',
-                 '{{ cookiecutter.project_slug_no_hyphen }}/{{ cookiecutter.project_slug_no_hyphen }}/data/xgboost_test_data.tsv',
-                 '{{ cookiecutter.project_slug_no_hyphen }}/{{ cookiecutter.project_slug_no_hyphen }}/models/xgboost_test_model.xgb']
+cli_path = '{{ cookiecutter.project_slug_no_hyphen/cookiecutter.project_slug_no_hyphen'
+
+pytorch_files = [f'{cli_path}/cli_pytorch.py']
+
+tensorflow_files = [f'{cli_path}/cli_tensorflow.py']
+
+xgboost_files = [f'{cli_path}/cli_xgboost.py',
+                 '{{ cookiecutter.project_slug_no_hyphen }}/data/xgboost_test_data.tsv',
+                 '{{ cookiecutter.project_slug_no_hyphen }}/models/xgboost_test_model.xgb']
 
 is_pytorch = '{{ cookiecutter.framework }}' == 'pytorch'
 is_tensorflow = '{{ cookiecutter.framework }}' == 'tensorflow'
 is_xgboost = '{{ cookiecutter.framework }}' == 'xgboost'
 
 if not is_pytorch:
-    pass
+    for file in pytorch_files:
+        remove(os.path.join(os.getcwd(), file))
 
 if not is_tensorflow:
-    pass
+    for file in tensorflow_files:
+        remove(os.path.join(os.getcwd(), file))
 
 if not is_xgboost:
     for file in xgboost_files:

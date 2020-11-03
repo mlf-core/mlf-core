52ebb8323f1f2957bf3f07938973c0ab5add0045
Zethson <lukas.heumos@posteo.net>
2020-11-03 13:15:53 +0100

    added package-prediction TF and Pytorch
    Signed-off-by: Zethson <lukas.heumos@posteo.net>

--- mlf_core/create/templates/package/package_prediction/{{ cookiecutter.project_slug_no_hyphen }}/{{cookiecutter.project_slug_no_hyphen}}/cli_tensorflow.py
+++ mlf_core/create/templates/package/package_prediction/{{ cookiecutter.project_slug_no_hyphen }}/{{cookiecutter.project_slug_no_hyphen}}/cli_tensorflow.py
@@ -1,10 +1,9 @@
 import os
 import sys
-from dataclasses import dataclass
 
 import click
 import numpy as np
-import xgboost as xgb
+import tensorflow as tf
 
 from rich import traceback, print
 
@@ -25,56 +24,27 @@ def main(input: str, model: str, cuda: bool, output: str):
 
     print('[bold blue]Run [green]{{ cookiecutter.project_name }} --help [blue]for an overview of all commands\n')
     if not model:
-        model = get_xgboost_model(f'{WD}/models/xgboost_test_model.xgb')
+        model = get_tensorflow_model(f'{WD}/models/tensorflow_test_model')
     else:
-        model = get_xgboost_model(model)
+        model = get_tensorflow_model(model)
     if cuda:
-        model.set_param({'predictor': 'gpu_predictor'})
+        model.cuda()
     print('[bold blue] Parsing data')
-    data_to_predict = parse_data_to_predict(input)
+    data_to_predict = read_data_to_predict(input)
     print('[bold blue] Performing predictions')
-    predictions = np.round(model.predict(data_to_predict.DM))
+    predictions = np.round(model.predict(data_to_predict))
     print(predictions)
     if output:
         print(f'[bold blue]Writing predictions to {output}')
         write_results(predictions, output)
 
 
-@dataclass
-class Dataset:
-    X: np.ndarray
-    y: list
-    DM: xgb.DMatrix
-    gene_names: list
-    sample_names: list
-
-
-def parse_data_to_predict(path_to_data_to_predict: str) -> Dataset:
+def read_data_to_predict(path_to_data_to_predict: str):
     """
     Parses the data to predict and returns a full Dataset include the DMatrix
     :param path_to_data_to_predict: Path to the data on which predictions should be performed on
     """
-    X = []
-    y = []
-    gene_names = []
-    sample_names = []
-    with open(path_to_data_to_predict, "r") as file:
-        all_runs_info = next(file).split("\n")[0].split("\t")[2:]
-        for run_info in all_runs_info:
-            split_info = run_info.split("_")
-            y.append(int(split_info[0]))
-            sample_names.append(split_info[1])
-        for line in file:
-            split = line.split("\n")[0].split("\t")
-            X.append([float(x) for x in split[2:]])
-            gene_names.append(split[:2])
-
-    X = [list(i) for i in zip(*X)]
-
-    X_np = np.array(X)
-    DM = xgb.DMatrix(X_np, label=y)
-
-    return Dataset(X_np, y, DM, gene_names, sample_names)
+    return
 
 
 def write_results(predictions: np.ndarray, path_to_write_to) -> None:
@@ -83,18 +53,20 @@ def write_results(predictions: np.ndarray, path_to_write_to) -> None:
     :param predictions: Predictions as a numpy array
     :param path_to_write_to: Path to write the predictions to
     """
-    np.savetxt(path_to_write_to, predictions, delimiter=',')
+    pass
 
 
-def get_xgboost_model(path_to_xgboost_model: str):
+def get_tensorflow_model(path_to_tensorflow_model: str):
     """
     Fetches the model of choice and creates a booster from it.
-    :param path_to_xgboost_model: Path to the xgboost model1
+    :param path_to_tensorflow_model: Path to the xgboost model1
     """
-    model = xgb.Booster()
-    model.load_model(os.path.abspath(path_to_xgboost_model))
+    strategy = tf.distribute.MirroredStrategy()
+    # Loading the model using lower level API
+    with strategy.scope():
+        model = tf.saved_model.load(path_to_tensorflow_model)
 
-    return model
+        return model
 
 
 if __name__ == "__main__":

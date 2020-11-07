6acdcd26c2685fecbacb9cd5af4b8e483f1a37d1
Zethson <lukas.heumos@posteo.net>
2020-11-07 18:46:48 +0100

    Bump version from 1.7.1 to 1.7.2

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mlf-core/mlf-core',
-    version='1.7.1',
+    version='1.7.2',
     zip_safe=False,
 )

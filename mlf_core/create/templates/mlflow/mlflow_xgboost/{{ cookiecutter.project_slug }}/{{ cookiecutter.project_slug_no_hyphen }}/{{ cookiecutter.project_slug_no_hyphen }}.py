c3085416fc776cb30ea1b0fcb1e08d0e809d09b0
Imipenem <philipp_ehm@protonmail.com>
2021-01-29 21:31:06 +0100

    Removed click, added argparse for all templates

--- mlf_core/create/templates/mlflow/mlflow_xgboost/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug_no_hyphen }}/{{ cookiecutter.project_slug_no_hyphen }}.py
+++ mlf_core/create/templates/mlflow/mlflow_xgboost/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug_no_hyphen }}/{{ cookiecutter.project_slug_no_hyphen }}.py
@@ -1,29 +1,55 @@
-import click
+from argparse import ArgumentParser
 import xgboost as xgb
 import mlflow
 import mlflow.xgboost
 import time
 import GPUtil
-
-from rich import traceback
+from rich import traceback, print
 
 from mlf_core.mlf_core import log_sys_intel_conda_env, set_general_random_seeds
 from data_loading.data_loader import load_train_test_data
 
 
-@click.command()
-@click.option('--max_epochs', type=int, default=25, help='Number of epochs to train')
-@click.option('--general-seed', type=int, default=0, help='General Python, Python random and Numpy seed.')
-@click.option('--xgboost-seed', type=int, default=0, help='XGBoost specific random seed.')
-@click.option('--cuda', type=click.Choice(['True', 'False']), default=True, help='Enable or disable CUDA support.')
-@click.option('--single-precision-histogram', default=True, help='Enable or disable single precision histogram calculation.')
-def start_training(max_epochs, general_seed, xgboost_seed, cuda, single_precision_histogram):
+def start_training():
+    parser = ArgumentParser(description="XGBoost Example")
+    parser.add_argument(
+        "--max_epochs",
+        type=int,
+        default=25,
+        help="Number of epochs to train",
+    )
+    parser.add_argument(
+        "--general-seed",
+        type=int,
+        default=0,
+        help="General Python, Python random and Numpy seed.",
+    )
+    parser.add_argument(
+        "--xgboost-seed",
+        type=int,
+        default=0,
+        help="XGBoost specific random seed.",
+    )
+    parser.add_argument(
+        "--cuda",
+        type=bool,
+        default=True,
+        help="Enable or disable CUDA support.",
+    )
+    parser.add_argument(
+        "--single-precision-histogram",
+        type=bool,
+        default=True,
+        help="Enable or disable single precision histogram calculation.",
+    )
     avail_gpus = GPUtil.getGPUs()
-    use_cuda = True if cuda == 'True' and len(avail_gpus) > 0 else False
+    args = parser.parse_args()
+    dict_args = vars(args)
+    use_cuda = True if dict_args['cuda'] == 'True' and len(avail_gpus) > 0 else False
     if use_cuda:
-        click.echo(click.style(f'Using {len(avail_gpus)} GPUs!', fg='blue'))
+        print(f'[bold blue]Using {len(avail_gpus)} GPUs!')
     else:
-        click.echo(click.style('No GPUs detected. Running on the CPU', fg='blue'))
+        print('[bold blue]No GPUs detected. Running on the CPU')
 
     with mlflow.start_run():
         # Fetch and prepare data
@@ -35,14 +61,14 @@ def start_training(max_epochs, general_seed, xgboost_seed, cuda, single_precisio
         # Set XGBoost parameters
         param = {'objective': 'multi:softmax',
                  'num_class': 8,
-                 'single_precision_histogram': True if single_precision_histogram == 'True' else False,
+                 'single_precision_histogram': True if dict_args['single_precision_histogram'] == 'True' else False,
                  'subsample': 0.5,
                  'colsample_bytree': 0.5,
                  'colsample_bylevel': 0.5}
 
         # Set random seeds
-        set_general_random_seeds(general_seed)
-        set_xgboost_random_seeds(xgboost_seed, param)
+        set_general_random_seeds(dict_args['general_seed'])
+        set_xgboost_random_seeds(dict_args['xgboost_seed'], param)
 
         # Set CPU or GPU as training device
         if use_cuda:
@@ -53,10 +79,10 @@ def start_training(max_epochs, general_seed, xgboost_seed, cuda, single_precisio
         # Train on the chosen device
         results = {}
         runtime = time.time()
-        xgb.train(param, dtrain, max_epochs, evals=[(dtest, 'test')], evals_result=results)
+        xgb.train(param, dtrain, dict_args['max_epochs'], evals=[(dtest, 'test')], evals_result=results)
         device = 'GPU' if use_cuda else 'CPU'
         if use_cuda:
-            click.echo(click.style(f'{device} Run Time: {str(time.time() - runtime)} seconds', fg='green'))
+            print(f'[bold green]{device} Run Time: {str(time.time() - runtime)} seconds')
 
         # Log hardware and software
         log_sys_intel_conda_env()
--- mlf_core/create/templates/mlflow/mlflow_xgboost_dask/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
+++ mlf_core/create/templates/mlflow/mlflow_xgboost_dask/{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
@@ -1,12 +1,10 @@
-import platform
-
-import click
 import tempfile
 import mlflow
 import subprocess
 import os
 import numpy as np
 import random
+from rich import print
 
 
 def set_general_random_seeds(seed):
@@ -22,29 +20,23 @@ def log_sys_intel_conda_env():
 
 
 def log_system_intelligence(reports_output_dir: str):
-    current_platform = platform.system()
-    if current_platform != 'Linux':
-        click.echo(click.style(f'Running on {current_platform} which is not supported by system-intelligence. Skipping hardware report.', fg='red'))
-        click.echo(click.style('Run MLflow with Docker to enforce a Linux environment.', fg='blue'))
-        return
-
     # Scoped import to prevent issues like RuntimeError: Numba cannot operate on non-primary CUDA context
     from system_intelligence.query import query_and_export
 
-    click.echo(click.style(f'Writing reports locally to {reports_output_dir}\n', fg='blue'))
-    click.echo(click.style('Running system-intelligence', fg='blue'))
+    print(f'[bold blue]Writing reports locally to {reports_output_dir}\n')
+    print('[bold blue]Running system-intelligence')
     query_and_export(query_scope={'all'},
                      verbose=False,
                      export_format='json',
                      generate_html_table=True,
                      output=f'{reports_output_dir}/system_intelligence.json')
-    click.echo(click.style('Uploading system-intelligence report as a run artifact...', fg='blue'))
+    print('[bold blue]Uploading system-intelligence report as a run artifact...')
     mlflow.log_artifacts(reports_output_dir, artifact_path='reports')
 
 
 def log_conda_environment(reports_output_dir: str):
-    click.echo(click.style('Exporting conda environment...', fg='blue'))
+    print('[bold blue]Exporting conda environment...')
     conda_env_filehandler = open(f'{reports_output_dir}/{{ cookiecutter.project_slug_no_hyphen }}_conda_environment.yml', "w")
     subprocess.call(['conda', 'env', 'export', '--name', '{{ cookiecutter.project_slug_no_hyphen }}'], stdout=conda_env_filehandler)
-    click.echo(click.style('Uploading conda environment report as a run artifact...', fg='blue'))
+    print('[bold blue]Uploading conda environment report as a run artifact...')
     mlflow.log_artifact(f'{reports_output_dir}/{{ cookiecutter.project_slug_no_hyphen }}_conda_environment.yml', artifact_path='reports')

8f5a5b7c3df9865601090d9a2836cd326ad21722
Edmund Miller <edmund.a.miller@protonmail.com>
2021-02-02 22:10:10 -0600

    fix: Remove query_and_export import

--- mlf_core/create/templates/common_mlflow_files/{{ cookiecutter.common_mlflow }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
+++ mlf_core/create/templates/common_mlflow_files/{{ cookiecutter.common_mlflow }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
@@ -10,7 +10,6 @@ import torch
 {%- elif cookiecutter.language == "tensorflow" %}
 # ONLY FOR TENSORFLOW -> else RuntimeError: make_default_context() wasn't able to create a context on any of the 1 detected devices
 import tensorflow as tf
-from system_intelligence.query import query_and_export  # noqa F401
 {%- endif %}
 
 

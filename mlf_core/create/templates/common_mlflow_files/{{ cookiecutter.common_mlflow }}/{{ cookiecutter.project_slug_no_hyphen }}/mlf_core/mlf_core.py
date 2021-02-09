81197473b246ea2f53aa15d61b242809ec984be0
Edmund Miller <edmund.a.miller@protonmail.com>
2021-02-08 20:36:07 -0600

    style: Remove a log statement
    Co-authored-by: Kevin Menden <kevin.menden@t-online.de>

--- mlf_core/create/templates/common_mlflow_files/{{ cookiecutter.common_mlflow }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
+++ mlf_core/create/templates/common_mlflow_files/{{ cookiecutter.common_mlflow }}/{{ cookiecutter.project_slug_no_hyphen }}/mlf_core/mlf_core.py
@@ -106,5 +106,4 @@ class MLFCore:
     def log_input_data(input_data: str):
         print('[bold blue]Hashing input data...')
         input_hash = md5_sum_input_data(input_data)
-        print('[bold blue]Uploading input hash as a run param...')
         mlflow.log_param("input_hash", input_data + "-" + input_hash)

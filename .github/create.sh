d33603b9c1007205ed9ee1e0d10ffbba4b8fd823
zethson <lukas.heumos@posteo.net>
2021-04-25 18:22:51 +0200

    fix create WFs
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- .github/create.sh
+++ .github/create.sh
@@ -4,15 +4,15 @@ set -e
 case "$1" in
 
   pytorch)
-    echo -e "\n\n\n\n\nn\n\n\n\nn" | mlf-core create
+    echo -e "\n\n\n\n\nn\n\n\n\nn" | poetry run mlf-core create
     ;;
 
   tensorflow)
-    echo -e "\n\033[B\n\n\n\nn\n\n\n\nn" | mlf-core create
+    echo -e "\n\033[B\n\n\n\nn\n\n\n\nn" | poetry run mlf-core create
     ;;
 
   xgboost)
-    echo -e "\n\033[B\033[B\n\n\n\nn\n\n\n\nn" | mlf-core create
+    echo -e "\n\033[B\033[B\n\n\n\nn\n\n\n\nn" | poetry run mlf-core create
     ;;
 
   *)

68e521034271e032a49a6708d18be46320cf365b
zethson <lukas.heumos@posteo.net>
2021-04-25 18:40:25 +0200

    fix CI
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -91,7 +91,7 @@ def precommit(session: Session) -> None:
         "pep8-naming",
         "pre-commit",
         "pre-commit-hooks",
-        # "reorder-python-imports",
+        "reorder-python-imports",
     )
     session.run("pre-commit", *args)
     if args and args[0] == "install":
